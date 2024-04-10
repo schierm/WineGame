c5f6041cab4e5017282486ffae1f949282a4f621
mls8504 <meg.schier.123@gmail.com>
2024-04-10 00:11:22 -0400

    Added scenes + ui script for main menu
    three scenes created for each person's level. UI script handles transition from start to level select

--- Wine Game/Assets/Scripts/MainMenuScript.cs
+++ Wine Game/Assets/Scripts/MainMenuScript.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using Unity.VisualScripting;
 
 public class MainMenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject lvlSelect;
+    public GameObject mainMenu;
+    public Button startBtn;
+    public Button japanLvl;
+    public Button pennLvl;
+    public Button lebanonLvl;
+
+    private void Start()
     {
-        
+        lvlSelect.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowLvlSelect()
+    { 
+        mainMenu.SetActive(false);
+        lvlSelect.SetActive(true);
+    }  
+
+    public void ShowMainMenu()
     {
-        
+        mainMenu.SetActive(true);
+        lvlSelect.SetActive(false);
     }
+
+    public void ShowPennLvl() {
+        SceneManager.LoadScene(2);
+    } 
+
+    public void ShowLebanonLvl() {
+        SceneManager.LoadScene(3);
+    } 
+
+    public void ShowJapanLvl() {
+        SceneManager.LoadScene(1);
+    }
+
+
+    
 }
