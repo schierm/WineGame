c29920add640d39d1fe5af20d17ec303cfa226ce
JTyson02 <70408537+JTyson02@users.noreply.github.com>
2024-04-11 22:52:56 -0400

    Bug is fixed
    It's done

--- Wine Game/Assets/Scripts/QuestionLogic.cs
+++ Wine Game/Assets/Scripts/QuestionLogic.cs
@@ -31,10 +31,14 @@ public class QuestionLogic : MonoBehaviour
     {
 
         correctAnswers++;
-
+        
+       // Debug.Log("Correct answer " + currentQuestion);
+        
         flashScreen.gameObject.SetActive(true);
 
         flashScreen.correctAnswer = true;
+
+        
 
         SlideChange();
 
@@ -50,25 +54,31 @@ public class QuestionLogic : MonoBehaviour
 
         flashScreen.correctAnswer = false;
 
+        //Debug.Log("Incorrect Answer " + currentQuestion);
+
         SlideChange();
 
     }
 
     public void SlideChange()
     {
+        
         if (currentQuestion + 1 <= nextInfo.Length)
         {
+            Debug.Log("Slide Change " + currentQuestion);
             nextInfo[currentQuestion].SetActive(false);
 
             currentQuestion++;
-
+            Debug.Log("Slide Change " + currentQuestion);
             nextInfo[currentQuestion].SetActive(true);
+
+            Debug.Log("Next Info Length" + nextInfo.Length);
 
         }
         else
         {
-            Debug.Log(currentQuestion);
-            Debug.Log(nextInfo.Length);
+            Debug.Log("Slide Change " + currentQuestion);
+            Debug.Log("Next Info Length" + nextInfo.Length);
             nextInfo[currentQuestion].SetActive(false);
 
             endScreen.SetActive(true);
